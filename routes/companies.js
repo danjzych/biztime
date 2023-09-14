cec52bc8f1fc65967ec57c347e7f0dd3b874a545
Jung-GunSong <jsong781@gmail.com>
2023-09-14 15:18:22 -0400

    finished post and put routes

--- routes/companies.js
+++ routes/companies.js
@@ -37,5 +37,37 @@ router.get('/:code', async function (req, res) {
   })
 })
 
+router.post("/", async function(req,res){
+  const {code, name, description} = req.body;
+
+  const result = await db.query(
+    `INSERT into companies(code, name, description)
+     VALUES ($1,$2,$3)
+     RETURNING code, name, description
+    `, [code,name,description]);
+    const company = result.rows[0]
+
+    return res.status(201).json({company})
+})
+
+router.put("/:code", async function(req,res){
+  const code = req.params.code;
+  const {name, description} = req.body
+
+  const result = await db.query(
+   `UPDATE companies
+    SET name = $1,
+         description = $2
+    WHERE code = $3
+    RETURNING code, name, description
+    `, [name, description, code]
+  );
+  const company = result.rows[0]
+
+  if(company)return res.json({company});
+
+  throw new NotFoundError();
+
+})
 
 module.exports = router;
